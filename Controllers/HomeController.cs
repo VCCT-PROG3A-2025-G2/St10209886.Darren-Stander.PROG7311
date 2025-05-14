1a8d79dc2e1132ffea5b1de5871edcfe9c79a60b
Darren-Stander <darrenjadestander@gmail.com>
2025-05-14 14:48:54 +0200

    Detailed comments for the HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,24 +1,31 @@
+// Start of file
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 
 namespace TestApp.Controllers
 {
+    // Start of HomeController class
     public class HomeController : Controller
     {
         public IActionResult Index()
         {
+            // Check if a user is logged in (session contains Username)
             if (HttpContext.Session.GetString("Username") != null)
             {
+                // Pass username to the view and show logged-in layout
                 ViewBag.Username = HttpContext.Session.GetString("Username");
                 return View("LoggedIn");
             }
+            
             return View();
-
         }
 
         public IActionResult AccessDenied()
         {
+            // Display access denied page
             return View();
         }
     }
+    // End of HomeController class
 }
+// End of file
