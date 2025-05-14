79cf7ecbfe29da4ea2b35575be0c925ea1be9bf0
Darren-Stander <darrenjadestander@gmail.com>
2025-05-14 15:07:28 +0200

    commenst made to the product.cs

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,21 +1,32 @@
-﻿using System;
+﻿// Start of file
+using System;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace TestApp.Models
 {
+    // Start of Product class
     public class Product
     {
+        // Primary key for the product
         public int Id { get; set; }
+
+        // Name of the product
         public string Name { get; set; }
+
+        // Category of the product
         public string Category { get; set; }
+
+        // Date when the product was produced
         public DateTime ProductionDate { get; set; }
 
-        // 🔥 Foreign Key
+        // Foreign key linking to the owning Farmer
         public int FarmerId { get; set; }
 
-        // 🔥 Navigation Property (Validation Ignored)
-        [ValidateNever] // Ignore this during model validation
+        // Navigation property to the associated Farmer
+        [ValidateNever]
         public Farmer Farmer { get; set; }
     }
+    // End of Product class
 }
+// End of file
