bb0c3900f2a1b56f539b48d250943b1e1ff2aca8
Darren-Stander <darrenjadestander@gmail.com>
2025-05-14 15:06:12 +0200

    added commenst to farmer

--- Models/Farmer.cs
+++ Models/Farmer.cs
@@ -1,12 +1,24 @@
-﻿namespace TestApp.Models
+﻿// Start of file
+namespace TestApp.Models
 {
+    // Start of Farmer class
     public class Farmer
     {
+        // Primary key for Farmer
         public int Id { get; set; }
+
+        // Display name of the farmer
         public string Name { get; set; }
+
+        // Contact information (e.g., phone or email)
         public string ContactInfo { get; set; }
+
+        // Physical address of the farmer
         public string Address { get; set; }
 
+        // Foreign key linking to the User record
         public int UserId { get; set; }
     }
+    // End of Farmer class
 }
+// End of file
