be91a15f75eac97a42aeebede916422dc1a0ea50
Darren-Stander <darrenjadestander@gmail.com>
2025-05-14 15:09:13 +0200

    added comments so that the user is aware of the code.

--- Models/User.cs
+++ Models/User.cs
@@ -1,11 +1,24 @@
-﻿namespace TestApp.Models
+﻿// Start of file
+namespace TestApp.Models
 {
+    // Start of User class
     public class User
     {
+        // Primary key for the user
         public int Id { get; set; }
+
+        // Username used for login
         public string Username { get; set; }
+
+        // Password for authentication
         public string Password { get; set; }
+
+        // Role assigned to the user (e.g., "Farmer" or "Employee")
         public string Role { get; set; }
-        public int? FarmerId { get; set; } // New - Links to the Farmer Table
+
+        // Optional foreign key linking to a Farmer profile
+        public int? FarmerId { get; set; }
     }
+    // End of User class
 }
+// End of file
